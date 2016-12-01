be69fe154ff646305d246aae4ecaa9c56fbb2957
lilu-leo <li854425934@163.com>
2016-12-01 09:51:30 +0800

    change test.py name

--- data_analysis_primary/test.py
+++
@@ -1,9 +0,0 @@
-import unicodecsv
-enrollments = []
-with open('enrollments.csv', 'rb') as f:
-    reader = unicodecsv.DictReader(f)
-
-    for row in reader:
-        enrollments.append(row)
-            
-print enrollments[0]
